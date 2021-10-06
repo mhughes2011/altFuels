9014056029c9412a1a49a3170128e1fde85b0053
Max Hughes <mhughes2011@gmail.com>
2021-10-05 20:38:58 -0700

    created and implemented navbar component template

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Welcome to my page!</h1>
       </div>
     );
