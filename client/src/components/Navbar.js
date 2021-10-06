0acd3e41f37fbd1e7f186bdeb46b9be153530109
Max Hughes <mhughes2011@gmail.com>
2021-10-05 20:38:05 -0700

    started organizing components

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+function Navbar() {
+    return(
+        <Fragment>
+            <p>This is the navbar component!</p>
+        </Fragment>
+    )
+}
+
+export default Navbar;
