01d1729efc1796a22c34a4b518e753da30477cb5
Max Hughes <mhughes2011@gmail.com>
2021-10-06 17:49:54 -0700

    created a placeholder component for the hero image

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import './App.css';
 
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Hero />
         <h1>Welcome to my page!</h1>
       </div>
     );
---
+++ client/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+function Hero() {
+    return (
+        <Fragment>
+            <h2>This is the placeholder for the hero image</h2>
+        </Fragment>
+    )
+}
+
+export default Hero;
